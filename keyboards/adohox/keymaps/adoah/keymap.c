8d1659f571ebf48bf774d500905a923db12a63a6
Alex <xadoahx@gmail.com>
2018-06-23 11:26:52 +0300

    updated keymap.c

--- keyboards/adohox/keymaps/adoah/keymap.c
+++ keyboards/adohox/keymaps/adoah/keymap.c
@@ -9,16 +9,16 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QWERTY 0
-#define _LOWER 1
+#define _DVORAK 1
 #define _RAISE 2
-#define _DVORAK 3
+#define _LOWER 3
 #define _ADJUST 16
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
   DVORAK,
+  RAISE,
+  LOWER,
   ADJUST,
 };
 
@@ -29,6 +29,7 @@ enum custom_keycodes {
 // Defines for task manager and such
 #define CALTDEL LCTL(LALT(KC_DEL))
 #define TSKMGR LCTL(LSFT(KC_ESC))
+#define QUIT LALT(KC_F4)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -43,59 +44,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `----------------------------------'                        `----------------------------------'
  *              ,-----------------------------.       ,---------------------------.
  *              | Windows| LOWER|      |     |       |      |      | RAISE| Tab |
- *              `--------------| Space|Enter|       |BckSpc|Shift |------+------.
- *                            |      |     |       |      |      |
- *                            `------------'       `------------'
+ *              `--------------| Space |Enter|       |BckSpc|Shift |------+------.
+ *                            |        |     |       |      |      |
+ *                             `------------'        `------------'
  */
-[_QWERTY] = KEYMAP( \
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,              KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-          LT(KC_LCTRL, KC_LGUI), MO(LOWER), KC_SPC, KC_ENT,                     KC_BSPC, KC_LSHIFT, MO(RAISE), LT(KC_LALT, KC_TAB)               \
-),
 
-/* Raise
- *
- * ,----------------------------------.           ,----------------------------------.
- * |   1  |   2  |   3  |   4  |   5  |           |   6  |   7  |   8  |   9  |   0  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Tab | Left | Down |  Up  | Right|           |   -  |   (  |   )  |   [  |   ]  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Ctrl|   `  |  GUI |  Alt |DVORAK TOGGLE|    |   =  |   {  |   }  |   \  |   '  |
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  |      | LOWER|      |    |      | RAISE|      |
- *                  `-------------|      |    |      |------+------.
- *                                |      |    |      |
- *                                `------'    `------'
- */
-[_RAISE] = KEYMAP( \
-  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    \
-  KC_TAB,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,      KC_MINS, KC_LPRN, KC_RPRN,  KC_LBRC, KC_RBRC, \
-  KC_LCTL, KC_GRV,  KC_LGUI, KC_LALT, TG(DVORAK),      KC_EQL, KC_LCBR, KC_RCBR, KC_BSLS,  KC_QUOT, \
-          _______,  _______, _______, _______,      _______, _______, _______, _______            \
-),
-
-/* Lower
- *
- * ,----------------------------------.           ,----------------------------------.
- * |   !  |   @  |   #  |   $  |   %  |           |   ^  |   &  |   *  |   (  |   )  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Esc |      |      |      |      |           |      |   _  |   +  |   {  |   }  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Caps|   ~  |      |      |      |           |      |      |      |   |  |   "  |
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  |      | LOWER|      |    |      | RAISE|  Del |
- *                  `-------------|      |    | Enter|------+------.
- *                                |      |    |      |
- *                                `------'    `------'
- */
-[_LOWER] = KEYMAP( \
-  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, \
-  KC_ESC,  _______, _______, _______, _______,      _______, KC_UNDS, KC_PLUS, _______, _______, \
-  KC_CAPS, KC_TILD, _______, _______, _______,      _______, _______, _______, KC_PIPE,  KC_DQT, \
-          _______, _______, _______, _______,      KC_ENT,  _______, KC_DEL, _______              \
+  [_QWERTY] = KEYMAP( \
+  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
+  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
+  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
+          LT(_LOWER, KC_TAB), KC_SPC, KC_LCTRL, KC_LGUI,            KC_LALT, KC_LSFT, KC_BSPC, LT(_RAISE, KC_ENT)\
 ),
 
 /* Modded Dvorak
@@ -112,12 +70,56 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *              `--------------| Space|Enter|       |BckSpc|Shift |------+------.
  *                            |      |     |       |      |      |
  *                            `------------'       `------------'
- */
+*/
 [_DVORAK] = KEYMAP( \
   KC_SCLN,    KC_P,    KC_Y,    KC_F,    KC_G,              KC_C,    KC_R,    KC_L,    KC_V,    KC_Z,    \
   KC_A,    KC_E,    KC_O,    KC_U,    KC_I,              KC_D,    KC_H,    KC_T,    KC_N,    KC_S, \
   KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,              KC_M,    KC_W,    KC_COMM, KC_DOT,  KC_SLSH, \
-          LT(KC_LCTRL, KC_LGUI), MO(LOWER), KC_SPC, KC_ENT,                     KC_BSPC, KC_LSHIFT, MO(RAISE), LT(KC_LALT, KC_TAB)               \
+  LT(_LOWER, KC_TAB), KC_SPC, KC_LCTRL, KC_LGUI,            KC_LALT, KC_LSFT, KC_BSPC, LT(_RAISE, KC_ENT)   \
+),
+
+/* Raise
+ *
+ * ,----------------------------------.                  ,----------------------------------.
+ * |   1  |   2  |   3  |   4  |   5  |                  |   6  |   7  |   8  |   9  |   0  |
+ * |------+------+------+------+------|                  |------+------+------+------+------|
+ * |  DEL |   `  |  UP  |   :  |  ;   |                  |   -  |   (  |   )  |   [  |   ]  |
+ * |------+------+------+------+------|                  |------+------+------+------+------|
+ * |  Ctrl|   LEFT  |  DOWN |  RIGHT |DVORAK TOGGLE|     |   =  |   {  |   }  |   \  |   '  |
+ * `----------------------------------'                  `----------------------------------'
+ *                  ,--------------------.    ,------,-------------.
+ *                  |      | LOWER|      |    |      | RAISE|      |
+ *                  `-------------|      |    |      |------+------.
+ *                                |      |    |      |
+ *                                `------'    `------'
+ */
+[_RAISE] = KEYMAP( \
+  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    \
+  KC_DEL,    KC_GRV, KC_UP, KC_COLN,   KC_SCLN,      KC_MINS, KC_LPRN, KC_RPRN,  KC_LBRC, KC_RBRC, \
+  KC_LCTL, KC_LEFT,  KC_DOWN, KC_RIGHT, /*TG(DVORAK)*/ _______,      KC_EQL, KC_LCBR, KC_RCBR, KC_BSLS,  KC_QUOT, \
+          _______,  _______, _______, _______,      _______, _______, _______, _______            \
+),
+
+/* Lower
+ *
+ * ,----------------------------------.           ,----------------------------------.
+ * |   !  |   @  |   #  |   $  |   %  |           |   ^  |   &  |   *  |      |      |
+ * |------+------+------+------+------|           |------+------+------+------+------|
+ * |  Esc | lclk | msup | rclk |      |           |wh up |   _  |   +  |  del |      |
+ * |------+------+------+------+------|           |------+------+------+------+------|
+ * |  Caps|msleft|msdown|msrght|   ~  |           |wh dn |      | QUIT |   |  |   "  |
+ * `----------------------------------'           `----------------------------------'
+ *                  ,--------------------.    ,------,-------------.
+ *                  |      | LOWER|      |    |      | RAISE|  Del |
+ *                  `-------------|      |    | Enter|------+------.
+ *                                |      |    |      |
+ *                                `------'    `------
+  */
+[_LOWER] = KEYMAP( \
+  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, \
+  KC_ESC, KC_BTN1, KC_MS_U, KC_BTN2, _______,      KC_WH_U, KC_UNDS, KC_PLUS, KC_DEL, _______, \
+  KC_CAPS,  KC_MS_L, KC_MS_D, KC_MS_R, KC_TILD,      KC_WH_D, TG(_DVORAK), QUIT, KC_PIPE,  KC_DQT, \
+          _______, _______, _______, _______,      KC_ENT,  _______,  _______, _______              \
 ),
 
 /* Adjust (Lower + Raise)
@@ -125,9 +127,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------.           ,----------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |           |   F6 |  F7  |  F8  |  F9  |  F10 |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  F11 |  F12 |      |RGBSAI|RGBSAD|           |RGBVAD|      | Down |      |caltde|
+ * |  F11 |  F12 |      |RGBSAI|RGBSAD|           |RGBVAD|VolDn | PLAY |      |caltde|
  * |------+------+------+------+------|           |------+------+------+------+------|
- * | Reset|RGBTOG|RGBMOD|RGBHUI|RGBHUD|           |RGBVAI| Left | Down |Right |Taskmg|
+ * | Reset|RGBTOG|RGBMOD|RGBHUI|RGBHUD|           |RGBVAI|VolUp|MPrevi |MNext |Taskmg|
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,------,-------------.
  *                  |      | LOWER|      |    |      | RAISE|      |
@@ -137,8 +139,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] =  KEYMAP( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, \
-  KC_F11,  KC_F12,  _______, RGB_SAI, RGB_SAD,      RGB_VAD, _______, KC_UP, _______, CALTDEL, \
-  RESET,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD,      RGB_VAI, KC_LEFT, KC_DOWN, KC_RGHT,  TSKMGR, \
+  KC_F11,  KC_F12,  _______, RGB_SAI, RGB_SAD,      RGB_VAD, KC_VOLU, KC_MPLY, _______, CALTDEL, \
+  RESET,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD,      RGB_VAI, KC_VOLD, KC_MPRV, KC_MNXT,  TSKMGR, \
           _______,  _______, _______, _______,      _______,  _______, _______, _______        \
 )
 };
@@ -152,50 +154,46 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
-        persistant_default_layer_set(1UL<<_QWERTY);
+        set_single_persistent_default_layer(_QWERTY);
       }
       return false;
-      break;
-    case LOWER:
+    case DVORAK:
       if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        set_single_persistent_default_layer(_DVORAK);
       }
       return false;
-      break;
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        if (IS_LAYER_ON(_ADJUST)) {
+          layer_off(_RAISE);
+          layer_off(_LOWER);
+        }
       } else {
         layer_off(_RAISE);
+        if (IS_LAYER_ON(_ADJUST)) {
+          layer_off(_ADJUST);
+          layer_on(_LOWER);
+        }
+      }
+      return false;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case DVORAK:
-      if (record->event.pressed) {
-        layer_on(_DVORAK);
-//        update_quad_layer(_LOWER, _RAISE, _DVORAK, _ADJUST);
-      }
-      else
-      {
-        layer_off(_DVORAK);
-//        update_quad_layer(_LOWER, _RAISE, _DVORAK, _ADJUST);
-      }
-      return false;
-      break;
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
+        if (IS_LAYER_ON(_ADJUST)) {
+          layer_off(_LOWER);
+          layer_off(_RAISE);
+        }
       } else {
-        layer_off(_ADJUST);
+        layer_off(_LOWER);
+        if (IS_LAYER_ON(_ADJUST)) {
+          layer_off(_ADJUST);
+          layer_on(_RAISE);
+        }
       }
       return false;
-      break;
   }
   return true;
 }
